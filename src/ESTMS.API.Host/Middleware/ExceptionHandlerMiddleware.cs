9f78ba8766f5cf0c168c97b4adf3f8e167832ba5
ArnasBag <46868498+ArnasBag@users.noreply.github.com>
2023-04-01 17:00:09 +0300

    added starter exception handler

---
+++ src/ESTMS.API.Host/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ESTMS.API.Host.Middleware;
+
+public class ErrorDetails
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        switch (exception)
+        {
+            default:
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                break;
+        }
+
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+}
