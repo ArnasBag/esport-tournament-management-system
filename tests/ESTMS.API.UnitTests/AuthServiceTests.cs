eb417d5bf425c1323618535ff5f2b67eef1117f2
ArnasBag <46868498+ArnasBag@users.noreply.github.com>
2023-04-02 14:35:02 +0300

    added unit tests for auth service

---
+++ tests/ESTMS.API.UnitTests/AuthServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using ESTMS.API.Core.Exceptions;
+using ESTMS.API.DataAccess.Entities;
+using ESTMS.API.Services;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+
+namespace ESTMS.API.UnitTests;
+
+public class AuthServiceTests
+{
+    private Mock<IUserStore<ApplicationUser>> _userStoreMock;
+    private Mock<UserManager<ApplicationUser>> _userManagerMock;
+    private Mock<ITokenProvider> _tokenProviderMock;
+    private IAuthService _authService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+        _userManagerMock = new Mock<UserManager<ApplicationUser>>(
+            _userStoreMock.Object, null, null, null, null, null, null, null, null);
+        _userManagerMock.Object.UserValidators.Add(new UserValidator<ApplicationUser>());
+        _userManagerMock.Object.PasswordValidators.Add(new PasswordValidator<ApplicationUser>());
+
+        _tokenProviderMock = new Mock<ITokenProvider>();
+
+        _authService = new AuthService(_userManagerMock.Object, _tokenProviderMock.Object);
+    }
+
+    [Test]
+    public async Task LoginUserAsync_CorrectInput_AllDependenciesCalled()
+    {
+        _userManagerMock.Setup(x => x.FindByEmailAsync(
+            It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
+        _userManagerMock.Setup(x => x.CheckPasswordAsync(
+            It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(true);
+
+        await _authService.LoginUserAsync(It.IsAny<string>(), It.IsAny<string>());
+
+        _userManagerMock.Verify(x => x.FindByEmailAsync(It.IsAny<string>()), Times.Once);
+        _userManagerMock.Verify(x => x.CheckPasswordAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
+        _tokenProviderMock.Verify(x => x.GetToken(It.IsAny<string>()), Times.Once);
+    }
+
+    [Test]
+    public void LoginUserAsync_UserDoesNotExist_ThrowsException()
+    {
+        Assert.ThrowsAsync<BadRequestException>(() => _authService.LoginUserAsync(It.IsAny<string>(), It.IsAny<string>()));
+    }
+
+    [Test]
+    public void LoginUserAsync_PasswordDoesNotMatchEmail_ThrowsException()
+    {
+        _userManagerMock.Setup(x => x.FindByEmailAsync(
+            It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
+        _userManagerMock.Setup(x => x.CheckPasswordAsync(
+            It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(false);
+
+        Assert.ThrowsAsync<BadRequestException>(() => _authService.LoginUserAsync(It.IsAny<string>(), It.IsAny<string>()));
+    }
+
+    [Test]
+    public async Task RegisterUserAsync_CorrectInput_AllDependenciesCalled()
+    {
+        _userManagerMock.Setup(x => x.CreateAsync(
+            It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        await _authService.RegisterUserAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+        _userManagerMock.Verify(x => x.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
+    }
+
+    [Test]
+    public void RegisterUserAsync_UserAlreadyExists_ThrowsException()
+    {
+        _userManagerMock.Setup(x => x.CreateAsync(
+            It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed());
+
+        Assert.ThrowsAsync<BadRequestException>(() => _authService.RegisterUserAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+    }
+}
