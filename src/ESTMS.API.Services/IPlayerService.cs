91d135f05ef1ad9212159c686ac389ac10ce99b7
Jokūbas Keturakis <Jketura@gmail.com>
2023-04-17 10:21:53 +0300

    added Player service interface

---
+++ src/ESTMS.API.Services/IPlayerService.cs
@@ -0,0 +1,11 @@
+﻿using ESTMS.API.DataAccess.Entities;
+
+namespace ESTMS.API.Services;
+
+public interface IPlayerService
+{
+    Task<Player> CreatePlayerAsync(Player player);
+    Task<Player> UpdatePlayerAsync(int id, Player updatedPlayer);
+    Task<Player> GetPlayerByIdAsync(int id);
+    Task<List<Player>> GetAllPlayersAsync(string? userId = null);
+}
