afeace6cd7d271b0d60823c3d860d51bf9496d39
ArnasBag <46868498+ArnasBag@users.noreply.github.com>
2023-04-21 00:45:41 +0300

    added unit tests

--- tests/ESTMS.API.UnitTests/InvitationServiceTests.cs
+++ tests/ESTMS.API.UnitTests/InvitationServiceTests.cs
@@ -32,7 +32,7 @@ public class InvitationServiceTests
     [Test]
     public void ChangeInvitationStatusAsync_InvitationNotFound_ThrowsException()
     {
-        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()))
+        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>()))
             .ReturnsAsync(default(Invitation));
 
         Assert.ThrowsAsync<NotFoundException>(
@@ -42,7 +42,7 @@ public class InvitationServiceTests
     [Test]
     public void ChangeInvitationStatusAsync_InvitationAlreadyAccepted_ThrowsException()
     {
-        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()))
+        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>()))
             .ReturnsAsync(new Invitation { Status = InvitationStatus.Accepted });
 
         Assert.ThrowsAsync<BadRequestException>(
@@ -50,111 +50,296 @@ public class InvitationServiceTests
     }
 
     [Test]
-    public async Task ChangeInvitationStatusAsync_DeclinedInvitation_Ok()
+    public void ChangeInvitationStatus_SenderTriesToAccept_ThrowsException()
     {
-        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()))
-            .ReturnsAsync(new Invitation { Status = InvitationStatus.Pending });
+        string receiverUserId = "a";
+        string userIdWhoAccepts = "b";
 
-        await _invitationService.ChangeInvitationStatusAsync(It.IsAny<int>(), InvitationStatus.Declined);
+        _userIdProviderMock.Setup(x => x.UserId).Returns(userIdWhoAccepts);
+        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync(new Invitation
+            {
+                Receiver = new ApplicationUser
+                {
+                    Id = receiverUserId
+                }
+            });
 
-        _invitationRepositoryMock.Verify(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()), Times.Once);
-        _invitationRepositoryMock.Verify(x => x.UpdateInvitationAsync(It.IsAny<Invitation>()), Times.Once);
+        Assert.ThrowsAsync<BadRequestException>(
+            () => _invitationService.ChangeInvitationStatusAsync(It.IsAny<int>(), InvitationStatus.Accepted));
     }
 
     [Test]
-    public async Task ChangeInvitationStatusAsync_AcceptedInvitation_AddsPlayerToTeam()
+    public async Task ChangeInvitationStatus_TeamManagerAcceptsInvitation_Ok()
     {
-        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()))
-            .ReturnsAsync(new Invitation { Status = InvitationStatus.Pending });
+        string receiverUserId = "a";
+        string userIdWhoAccepts = "a";
+
+        _userIdProviderMock.Setup(x => x.UserId).Returns(userIdWhoAccepts);
+        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync(new Invitation
+            {
+                Receiver = new ApplicationUser
+                {
+                    Id = receiverUserId
+                },
+                Status = InvitationStatus.Pending,
+                Sender = new ApplicationUser()
+            });
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(default(Player));
 
         await _invitationService.ChangeInvitationStatusAsync(It.IsAny<int>(), InvitationStatus.Accepted);
 
-        _invitationRepositoryMock.Verify(x => x.GetInvitationByIdAsync(It.IsAny<int>(), It.IsAny<string>()), Times.Once);
+        _invitationRepositoryMock.Verify(x => x.GetInvitationByIdAsync(It.IsAny<int>()), Times.Once);
+        _invitationRepositoryMock.Verify(x => x.UpdateInvitationAsync(It.IsAny<Invitation>()), Times.Once);
+        _userRepositoryMock.Verify(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()), Times.Exactly(2));
+        _teamRepositoryMock.Verify(x => x.AssignPlayerToTeamAsync(It.IsAny<Team>(), It.IsAny<Player>()), Times.Once);
+    }
+
+    [Test]
+    public async Task ChangeInvitationStatus_PlayerAcceptsInvitation_Ok()
+    {
+        string receiverUserId = "a";
+        string userIdWhoAccepts = "a";
+
+        _userIdProviderMock.Setup(x => x.UserId).Returns(userIdWhoAccepts);
+        _invitationRepositoryMock.Setup(x => x.GetInvitationByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync(new Invitation
+            {
+                Receiver = new ApplicationUser
+                {
+                    Id = receiverUserId
+                },
+                Status = InvitationStatus.Pending,
+                Sender = new ApplicationUser()
+            });
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player());
+
+        await _invitationService.ChangeInvitationStatusAsync(It.IsAny<int>(), InvitationStatus.Accepted);
+
+        _invitationRepositoryMock.Verify(x => x.GetInvitationByIdAsync(It.IsAny<int>()), Times.Once);
         _invitationRepositoryMock.Verify(x => x.UpdateInvitationAsync(It.IsAny<Invitation>()), Times.Once);
         _userRepositoryMock.Verify(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()), Times.Once);
         _teamRepositoryMock.Verify(x => x.AssignPlayerToTeamAsync(It.IsAny<Team>(), It.IsAny<Player>()), Times.Once);
     }
 
     [Test]
-    public void CreateInvitationAsync_TeamNotFound_ThrowsException()
+    public void CreateInviteForTeamAsync_TeamNotFound_ThrowsException()
     {
         _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Team));
 
         Assert.ThrowsAsync<NotFoundException>(
-            () => _invitationService.CreateInvitationAsync(It.IsAny<int>(), It.IsAny<string>()));
+            () => _invitationService.CreateInviteForTeamAsync(It.IsAny<int>()));
     }
 
     [Test]
-    public void CreateInvitationAsync_PlayerNotFound_ThrowsException()
+    public void CreateInviteForTeamAsync_SenderAlreadyPartOfTheTeam_ThrowsException()
     {
-        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(default(Player));
+        int teamId = 1;
 
-        Assert.ThrowsAsync<NotFoundException>(
-            () => _invitationService.CreateInvitationAsync(It.IsAny<int>(), It.IsAny<string>()));
-    }
-
-    [Test]
-    public void CreateInvitationAsync_PlayerAlreadyInATeam_ThrowsException()
-    {
-        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team());
-
-        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()))
-            .ReturnsAsync(new Player { Team = new Team() });
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            Id = teamId,
+            TeamManager = new TeamManager
+            {
+                ApplicationUser = new ApplicationUser()
+            }
+        });
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player
+        {
+            Team = new Team
+            {
+                Id = teamId
+            }
+        });
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
 
         Assert.ThrowsAsync<BadRequestException>(
-            () => _invitationService.CreateInvitationAsync(It.IsAny<int>(), It.IsAny<string>()));
+            () => _invitationService.CreateInviteForTeamAsync(It.IsAny<int>()));
     }
 
     [Test]
-    public void CreateInvitationAsync_InviteToOtherTeam_ThrowsException()
+    public void CreateInviteForTeamAsync_SenderAlreadyPartOtherTeam_ThrowsException()
     {
-        string senderTeamManagerUserId = "id";
-        string requestedTeamTeamManagerUserId = "notid";
+        int teamId = 1;
+        int otherTeamId = 2;
 
-        _userIdProviderMock.Setup(x => x.UserId).Returns(senderTeamManagerUserId);
-        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>()))
-            .ReturnsAsync(new Team
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            Id = teamId,
+            TeamManager = new TeamManager
             {
-                TeamManager = new TeamManager
-                {
-                    ApplicationUser = new ApplicationUser
-                    {
-                        Id = requestedTeamTeamManagerUserId
-                    }
-                }
-            });
-        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()))
-            .ReturnsAsync(new Player());
+                ApplicationUser = new ApplicationUser()
+            }
+        });
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player
+        {
+            Team = new Team
+            {
+                Id = otherTeamId
+            }
+        });
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
 
-        Assert.ThrowsAsync<ForbiddenException>(
-            () => _invitationService.CreateInvitationAsync(It.IsAny<int>(), It.IsAny<string>()));
+        Assert.ThrowsAsync<BadRequestException>(
+            () => _invitationService.CreateInviteForTeamAsync(It.IsAny<int>()));
     }
 
     [Test]
-    public async Task CreateInvitationAsync_Ok()
+    public async Task CreateInviteForTeamAsync_ValidData_Ok()
     {
-        string teamManagerUserId = "id";
-
-        _userIdProviderMock.Setup(x => x.UserId).Returns(teamManagerUserId);
-        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>()))
-            .ReturnsAsync(new Team
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            Id = 1,
+            TeamManager = new TeamManager
             {
-                TeamManager = new TeamManager
-                {
-                    ApplicationUser = new ApplicationUser
-                    {
-                        Id = teamManagerUserId
-                    }
-                }
-            });
-        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()))
-            .ReturnsAsync(new Player());
+                ApplicationUser = new ApplicationUser()
+            }
+        });
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player());
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
 
-        await _invitationService.CreateInvitationAsync(It.IsAny<int>(), It.IsAny<string>());
+        await _invitationService.CreateInviteForTeamAsync(It.IsAny<int>());
 
         _teamRepositoryMock.Verify(x => x.GetTeamByIdAsync(It.IsAny<int>()), Times.Once);
+        _userRepositoryMock.Verify(x => x.GetUserByIdAsync(It.IsAny<string>()), Times.Exactly(2));
         _userRepositoryMock.Verify(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()), Times.Once);
         _invitationRepositoryMock.Verify(x => x.CreateInvitationAsync(It.IsAny<Invitation>()), Times.Once);
+
+    }
+
+    [Test]
+    public void CreateInviteForPlayerAsync_TeamNotFound_ThrowsException()
+    {
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(default(Team));
+
+        Assert.ThrowsAsync<NotFoundException>(
+            () => _invitationService.CreateInviteForPlayerAsync(It.IsAny<string>(), It.IsAny<int>()));
+    }
+
+    [Test]
+    public void CreateInviteForPlayerAsync_InviteToTeamWithNoAccess_ThrowsException()
+    {
+        string teamManagerUserId = "a";
+        string teamTeamManagerUserId = "b";
+
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            TeamManager = new TeamManager
+            {
+                ApplicationUser = new ApplicationUser
+                {
+                    Id = teamTeamManagerUserId
+                }
+            }
+        });
+        _userIdProviderMock.Setup(x => x.UserId).Returns(teamManagerUserId);
+
+        Assert.ThrowsAsync<UnauthorizedAccessException>(
+            () => _invitationService.CreateInviteForPlayerAsync(It.IsAny<string>(), It.IsAny<int>()));
+    }
+
+    [Test]
+    public void CreateInviteForPlayerAsync_PlayerNotFound_ThrowsException()
+    {
+        string teamManagerUserId = "a";
+        string teamTeamManagerUserId = "a";
+
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            TeamManager = new TeamManager
+            {
+                ApplicationUser = new ApplicationUser
+                {
+                    Id = teamTeamManagerUserId
+                }
+            }
+        });
+        _userIdProviderMock.Setup(x => x.UserId).Returns(teamManagerUserId);
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(default(ApplicationUser));
+
+        Assert.ThrowsAsync<NotFoundException>(
+            () => _invitationService.CreateInviteForPlayerAsync(It.IsAny<string>(), It.IsAny<int>()));
+    }
+
+    [Test]
+    public void CreateInviteForPlayerAsync_PlayerAlreadyBelongsToATeam_ThrowsException()
+    {
+        string teamManagerUserId = "a";
+        string teamTeamManagerUserId = "a";
+
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            TeamManager = new TeamManager
+            {
+                ApplicationUser = new ApplicationUser
+                {
+                    Id = teamTeamManagerUserId
+                }
+            }
+        });
+        _userIdProviderMock.Setup(x => x.UserId).Returns(teamManagerUserId);
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player
+        {
+            Team = new Team()
+        });
+
+        Assert.ThrowsAsync<BadRequestException>(
+            () => _invitationService.CreateInviteForPlayerAsync(It.IsAny<string>(), It.IsAny<int>()));
+    }
+
+    [Test]
+    public async Task CreateInviteForPlayerAsync_ValidData_Ok()
+    {
+        string teamManagerUserId = "a";
+        string teamTeamManagerUserId = "a";
+
+        _teamRepositoryMock.Setup(x => x.GetTeamByIdAsync(It.IsAny<int>())).ReturnsAsync(new Team
+        {
+            TeamManager = new TeamManager
+            {
+                ApplicationUser = new ApplicationUser
+                {
+                    Id = teamTeamManagerUserId
+                }
+            }
+        });
+        _userIdProviderMock.Setup(x => x.UserId).Returns(teamManagerUserId);
+        _userRepositoryMock.Setup(x => x.GetUserByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser());
+        _userRepositoryMock.Setup(x => x.GetPlayerByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new Player());
+
+        await _invitationService.CreateInviteForPlayerAsync(It.IsAny<string>(), It.IsAny<int>());
+
+        _teamRepositoryMock.Verify(x => x.GetTeamByIdAsync(It.IsAny<int>()), Times.Once);
+        _userRepositoryMock.Verify(x => x.GetUserByIdAsync(It.IsAny<string>()), Times.Once);
+        _userRepositoryMock.Verify(x => x.GetPlayerByUserIdAsync(It.IsAny<string>()), Times.Once);
+        _invitationRepositoryMock.Verify(x => x.CreateInvitationAsync(It.IsAny<Invitation>()), Times.Once);
+    }
+
+    [Test]
+    public void GetAllInvitationsAsync_BothFiltersFalse_ThrowsException()
+    {
+        Assert.ThrowsAsync<BadRequestException>(
+            () => _invitationService.GetAllInvitationsAsync(false, false));
+    }
+
+
+    [Test]
+    public async Task GetAllInvitationsAsync_SentTrueCallsGetAllSentInvitations_ThrowsException()
+    {
+        await _invitationService.GetAllInvitationsAsync(true, false);
+
+        _invitationRepositoryMock.Verify(x => x.GetAllSentInvitations(It.IsAny<string>()), Times.Once);
+    }
+
+
+    [Test]
+    public async Task GetAllInvitationsAsync_ReceivedTrueCallsGetAllReceivedInvitations_ThrowsException()
+    {
+        await _invitationService.GetAllInvitationsAsync(false, true);
+
+        _invitationRepositoryMock.Verify(x => x.GetAllReceivedInvitations(It.IsAny<string>()), Times.Once);
     }
 }
 
