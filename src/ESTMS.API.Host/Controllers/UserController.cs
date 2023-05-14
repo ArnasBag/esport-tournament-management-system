27e797b37976e11e7d18b70397c977d3c81b0d35
ArnasBag <46868498+ArnasBag@users.noreply.github.com>
2023-05-14 12:31:59 +0300

    fixed blob storage connection string in github showing

--- src/ESTMS.API.Host/Controllers/UserController.cs
+++ src/ESTMS.API.Host/Controllers/UserController.cs
@@ -56,6 +56,7 @@ public class UserController : ControllerBase
     }
 
     [HttpPut("{id}/role")]
+    [Authorize(Roles = Roles.Admin)]
     public async Task<IActionResult> ChangeUserRole(string id, [FromBody] ChangeUserRoleRequest role)
     {
         await _userService.ChangeUserRoleAsync(id, role.Role);
